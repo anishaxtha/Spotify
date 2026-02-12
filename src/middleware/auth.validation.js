227205a9319e06dea528f4e51c793598a12bf20b
Anisha Nayaju <anishanayaju26@gmail.com>
2026-02-12 19:37:01 +0545

    validation done

---
+++ src/middleware/auth.validation.js
@@ -0,0 +1,36 @@
+const { body, validationResult } = require("express-validator");
+
+function validateResult(req, res, next) {
+  const errors = validationResult(req);
+
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() });
+  }
+
+  next();
+}
+
+const registerUserValidationRules = [
+  body("username")
+    .isString()
+    .withMessage("Username must be a string")
+    .isLength({ min: 3, max: 20 })
+    .withMessage("Username must be between 3 and 20 characters"),
+
+  body("email")
+    .isEmail()
+    .withMessage("Invalid email format"),
+
+  body("password")
+    .isLength({ min: 6 })
+    .withMessage("Password must be at least 6 characters long"),
+
+  body("role")
+    .optional()
+    .isIn(["user", "admin"])
+    .withMessage("Role must be either user or admin"),
+
+  validateResult 
+];
+
+module.exports = { registerUserValidationRules };
--- src/routes/auth.route.js
+++ src/routes/auth.route.js
@@ -1,11 +1,11 @@
 const express = require("express")
 const authController = require("../controllers/auth.controller")
-
+const validationRules = require("../middleware/auth.validation")
 
 
 const router = express.Router()
 0
-router.post("/register", authController.registerUser)
+router.post("/register",validationRules.registerUserValidationRules, authController.registerUser)
 
 router.post("/login", authController.loginUser)
 
